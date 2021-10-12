cb67a637c64d023b7aa261075c5738361754e9ea
hariszafar1998 <86205356+hariszafar1998@users.noreply.github.com>
2021-10-12 02:09:58 -0700

    ..
    ..

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,11 +1,23 @@
+import React, { Component, Fragment } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
-  );
-}
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h1>Hello World!</h1>
+        <h2>hi</h2>
+      </Fragment>
+    );
+  }
+};
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hello World!</h1>
+//     </div>
+//   );
+// }
 
 export default App;
